0bd9bc640fad9428de648b9cb0252495b0ece458
adrianag. <adrianag.@mac>
2025-01-29 21:11:28 -0500

    created two variables and implemented a method

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -4,5 +4,14 @@ public class DemoTest2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		int a = 10;
+		int b = 2;
+	}
+	
+	public void divide(int a, int b) {
+		
+		System.out.println(a/b);
+		
 	}
 }
